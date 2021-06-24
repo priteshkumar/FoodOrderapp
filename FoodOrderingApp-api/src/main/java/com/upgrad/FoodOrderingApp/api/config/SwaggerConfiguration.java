8a5569bf94c9a1f7fda129b7f99645f9e1ee1163
Pritesh Kumar <pritesh.kumar2@gmail.com>
2021-06-24 22:03:50 +0530

    add test

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/config/SwaggerConfiguration.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/config/SwaggerConfiguration.java
@@ -18,6 +18,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket swagger() {
+        System.out.println("test log");
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.upgrad.FoodOrderingApp.api.controller")).paths(PathSelectors.any()).build();
     }
 }
